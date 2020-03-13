a59b597ac91d2ec091a44b8b7306f6658ae94bdf
Brad Ruud <bradruud@gmail.com>
2020-03-13 01:38:53 +0000

    adding breedFetcherTest.js file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,25 @@
 const request = require('request');
 const fs = require('fs');
-const input = process.argv.slice(2);
+// const input = process.argv.slice(2);
 const link = ('https://api.thecatapi.com/v1/breeds/search?q=');
 
+const fetchBreedDescription = function(breedName, callback) {
 
 
-request(link + input, (error, response, body) => {
- 
-  const data = JSON.parse(body);
-  
-  if (data.length === 0) {
-    return console.log(`That breed is not found, please try again!`);
-  }
+  request(link + breedName, (error, response, body) => {
+
     
-  console.log(data[0].description);
   
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    const data = JSON.parse(body);
 
-});
+    if (data.length === 0) {
+      callback(error, "");
+      return;
+    }
+
+    callback(error, data[0].description);
+
+  });
+};
+
+module.exports = { fetchBreedDescription };
